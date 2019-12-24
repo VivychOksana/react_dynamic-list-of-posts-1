9b539ce53f6ae8f6291059ea901a946a9e8612ae
Vivych Oksana <vivichokcana@gmail.com>
2019-12-24 14:18:12 +0200

    Updates

---
+++ precache-manifest.62a6448620080457bf38e5e21a35a4ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "047f23eaa7087ab34a89672380e00462",
+    "url": "/react_dynamic-list-of-posts-1/index.html"
+  },
+  {
+    "revision": "b09dabd567558b5224c3",
+    "url": "/react_dynamic-list-of-posts-1/static/css/main.7752958b.chunk.css"
+  },
+  {
+    "revision": "56ca03f88e34a54573be",
+    "url": "/react_dynamic-list-of-posts-1/static/js/2.15c2d62f.chunk.js"
+  },
+  {
+    "revision": "b09dabd567558b5224c3",
+    "url": "/react_dynamic-list-of-posts-1/static/js/main.a7cbe888.chunk.js"
+  },
+  {
+    "revision": "8f6e9e62ff2b48fcc047",
+    "url": "/react_dynamic-list-of-posts-1/static/js/runtime~main.9778cafb.js"
+  }
+]);
